6e51889339e32785fad78610385d0e09615f14d7
Kyle Zhou <kylezhou2002@hotmail.com>
2015-09-19 13:02:01 -0400

    activated logging

--- settings_azure.py
+++ settings_azure.py
@@ -4,3 +4,27 @@ ALLOWED_HOSTS = ["munchee.azurewebsites.net", "mchee.co"]
 SOCIAL_AUTH_LINKEDIN_OAUTH2_KEY = os.environ["LINKEDIN_KEY"]
 SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET = os.environ["LINKEDIN_SECRET"]
 SECRET_KEY = os.environ["DJANGO_KEY"]
+
+### log Django errors to the root of your Azure Website
+LOGGING = {
+  'version': 1,
+  'disable_existing_loggers': False,
+  'filters': {
+    'require_debug_false': {
+      '()': 'django.utils.log.RequireDebugFalse'
+    }
+  },
+  'handlers': {
+    'logfile': {
+      'class': 'logging.handlers.WatchedFileHandler',
+      'filename': 'D:/home/site/wwwroot/error.log'
+    },
+  },
+  'loggers': {
+    'django': {
+      'handlers': ['logfile'],
+      'level': 'ERROR',
+      'propagate': False,
+    },
+  }
+}
