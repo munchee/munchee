2968e4a230fc16ef4043a00b548091e55f4bbd22
Reinaesaya <xseanhylx@gmail.com>
2015-09-19 14:49:30 -0400

    Removed temporary fies and moved settings to proper place

--- munchee/views.py
+++ munchee/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.http import HttpResponseRedirect
+from .forms import *
 from .models import *
 
 # Create your views here.
