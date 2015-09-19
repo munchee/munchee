b9392a33977492f4c42ee9041da579809a3f763d
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-09-19 10:28:35 -0400

    Integrate basic LinkedIn login functionality

--- munchee/urls.py
+++ munchee/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from munchee import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home, name='home'),
+    url('', include('social.apps.django_app.urls', namespace='social'))
 ]
