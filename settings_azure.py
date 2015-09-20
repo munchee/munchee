d6504e38959c3b19ab635f7660d5b7b1df2560dc
Kyle Zhou <kylezhou2002@hotmail.com>
2015-09-20 04:28:41 -0400

    some server deployment stuff

--- settings_azure.py
+++ settings_azure.py
@@ -1,4 +1,3 @@
-import os
 from munchee.settings import *
 # Azure prod-specific variables config
 DEBUG = False
@@ -10,25 +9,26 @@ STATIC_ROOT = '/var/www/munchee/static'
 RETURN_URL = "http://munchee.cloudapp.net/complete/linkedin-oauth2/"
 
 ### log Django errors to the root of your Azure Website
-LOGGING = {
-  'version': 1,
-  'disable_existing_loggers': False,
-  'filters': {
-    'require_debug_false': {
-      '()': 'django.utils.log.RequireDebugFalse'
-    }
-  },
-  'handlers': {
-    'logfile': {
-      'class': 'logging.handlers.WatchedFileHandler',
-      'filename': 'D:/home/site/wwwroot/error.log'
-    },
-  },
-  'loggers': {
-    'django': {
-      'handlers': ['logfile'],
-      'level': 'ERROR',
-      'propagate': False,
-    },
-  }
-}
+#LOGGING = {
+#  'version': 1,
+#  'disable_existing_loggers': False,
+#  'filters': {
+#    'require_debug_false': {
+#      '()': 'django.utils.log.RequireDebugFalse'
+#    }
+#  },
+#  'handlers': {
+#    'logfile': {
+#      'class': 'logging.handlers.WatchedFileHandler',
+#      'filename': 'D:/home/site/wwwroot/error.log'
+#    },
+#  },
+#  'loggers': {
+#    'django': {
+#      'handlers': ['logfile'],
+#      'level': 'ERROR',
+#      'propagate': False,
+#    },
+#  }
+#}
+#
